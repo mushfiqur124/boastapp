663d04aa53692f62a443f3a525beaf28c72feba2
mushfiqur124 <mrahm228@gmail.com>
2024-05-26 22:44:32 -0400

    swift project creation

---
+++ BoastApp/BoastApp/BoastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoastAppApp.swift
+//  BoastApp
+//
+//  Created by Mushfiqur Rahman on 2024-05-26.
+//
+
+import SwiftUI
+
+@main
+struct BoastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
